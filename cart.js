87d4c931830fe2ce5863324ea1c334a779772e62
Maximilian Moga <maximilianmoga@gmail.com>
2020-01-24 10:43:02 -0800

    update shopping cart

---
+++ cart.js
@@ -0,0 +1,18 @@
+const cart = [{
+    id: 'Lamborghini',
+    quantity: 1
+}, {
+    id: 'Porsche',
+    quantity: 1
+}, {
+    id: 'Ferrari',
+    quantity: 1
+}, {
+    id: 'Audi',
+    quantity: 1
+}, {
+    id:'Bmw',
+    quantity: 1
+}];
+
+export default cart;
