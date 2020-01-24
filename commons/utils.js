0947fb3e0746193cd46904dd269be17e79caa454
Maximilian Moga <maximilianmoga@gmail.com>
2020-01-24 11:56:46 -0800

    update more the lab

---
+++ commons/utils.js
@@ -0,0 +1,4 @@
+function FindById() {
+
+}
+export default FindById
--- test/findById.test.js
+++ test/findById.test.js
@@ -1,4 +1,10 @@
-const Lamborghini = {
+import findById from '../commons/utils.js';
+import cars from '../data/cars.js';
+
+const test = QUnit.test;
+
+test('findByIdTest', function(assert){
+const obj = {
     id: 'Lamborghini',
     name: 'Lamborghini',
     image: 'https://www.thesun.co.uk/wp-content/uploads/2018/11/Sun_Instagram_image_download_yiannimize_152719-1.jpg',
@@ -6,8 +12,8 @@ const Lamborghini = {
     category: 'super-car',
     price: '$ 250000.00',
     cost: '$ 150000.00',
-};
-const Porsche = {
+}
+const car = {
     id: 'Porsche',
     name: 'Porsche gt3 rs',
     image: 'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/15q1/654924/2016-porsche-911-gt3-rs-photos-and-info-news-car-and-driver-photo-656941-s-original.jpg?fill=2:1&resize=480:*',
@@ -15,16 +21,16 @@ const Porsche = {
     category: 'super-car',
     price: '$ 143000.00',
     cost: '$ 100000.00',
-};
-const Ferrari = {
-    id: 'Ferrari',
-    name: 'Ferrari LaFerrari',
-    image: 'https://file.kelleybluebookimages.com/kbb/images/content/editorial/slideshow/2014-ferrari-laferrari/658422_top-front_modifiche-600-001.jpg', 
-    description: '6.3l 949whp with v12 engine 2.4sec 0-60mph',
-    price: '$ 3.5M',
-    cost: '$1.5M',
-};
-const Audi = {
+}
+const car1 = {
+        id: 'Ferrari',
+        name: 'Ferrari LaFerrari',
+        image: 'https://file.kelleybluebookimages.com/kbb/images/content/editorial/slideshow/2014-ferrari-laferrari/658422_top-front_modifiche-600-001.jpg', 
+        description: '6.3l 949whp with v12 engine 2.4sec 0-60mph',
+        price: '$ 3.5M',
+        cost: '$1.5M',
+}
+const car2 = {
     id: 'Audi',
     name: 'Audi r8',
     image: 'https://www.thesun.co.uk/wp-content/uploads/2018/11/Sun_Instagram_image_download_yiannimize_152719-1.jpg',
@@ -32,8 +38,8 @@ const Audi = {
     category: 'super-car',
     price: '$ 250000.00',
     cost: '$ 150000.00',
-};
-const Bmw = {
+}
+const car3 = {
     id: 'Bmw',
     name: 'Bmw m4',
     image: 'https://www.automobilemag.com/uploads/sites/11/2018/10/2018-BMW-M4-Competition-Front-Three-Quarters-.jpg?fit=around%7C875:492',
@@ -42,11 +48,16 @@ const Bmw = {
     cost: '$ 60000',
 }
 
-const Cars = [
-    Lamborghini,
-    Porsche,
-    Ferrari,
-    Audi,
-    Bmw,
-];
-export default Cars;
+const array = cars
+const id = 'Lamborghini'
+const id = 'Porsche'
+const id = 'Ferrari'
+const id = 'Audi'
+const id = 'bmw'
+
+const foundObject = findById(array,id)
+
+
+
+assert.equal(foundObject, obj)
+});
